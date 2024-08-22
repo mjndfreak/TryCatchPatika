44cd07afc467f780f20e16c20f945bc1993a5b4e
mjndfreakk <cagdasergencc@gmail.com>
2024-08-22 19:34:11 +0300

    Initial commit

---
+++ TryCatchPatika/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try
+{
+    Console.Write("Enter an integer: ");
+    int number = Convert.ToInt16(Console.ReadLine());
+
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Input is not an integer");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    
+}
